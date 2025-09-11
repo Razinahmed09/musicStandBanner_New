0db48f6653b30a0890ab87f1213b1db683a89291
Razinahmed09 <141953648+Razinahmed09@users.noreply.github.com>
2025-09-11 23:04:47 +0500

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+hoverBox.addEventListener("mouseenter", () => {
+    hoverArea.style.display = "block";
+  });
+  hoverBox.addEventListener("mouseleave", () => {
+    // Delay hiding to check if mouse entered hoverArea
+    setTimeout(() => {
+      if (!hoverArea.matches(":hover")) {
+        hoverArea.style.display = "none";
+      }
+    }, 100);
+  });
+  
+  hoverArea.addEventListener("mouseleave", () => {
+    hoverArea.style.display = "none";
+  });
+
+
+
+
+
+
+
+
+
+  
